ffe66c0afc6d2b20f273fa5e53f33e427c1e3484
sowndaryalakshmi.S <110122641+sowndaryasrinivas@users.noreply.github.com>
2022-10-12 16:07:29 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,12 @@
+import java.util.*;  
+class user 
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in); 
+System.out.print("Enter a string: ");  
+String str= sc.nextLine();              //reads string   
+System.out.print("hi,welcome to this mentoring session "+str);  
+          
+}  
+}  
